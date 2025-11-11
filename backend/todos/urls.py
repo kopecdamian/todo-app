4898d1e36eabd364b822ebfae3ae0c9dda5af14d
damian9112 <kopec.damian@icloud.com>
2025-11-11 23:21:18 +0100

    feat: Add function of creating and editing task [#2]

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_tasks, name='all_tasks'),
+    path('task_create', views.task_create, name='task_create'),
+    path('<int:task_id>', views.task_details, name='task_details'),
 ]
--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+# All tasks
 def all_tasks(request):
     tasks = Task.objects.all()
     return render(request, 'todos/all_tasks.html', {'tasks': tasks})
+
+# Create new task
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect('all_tasks')
+    else:
+        form = TaskForm()
+    return render(request, 'todos/task_form.html', {'form': form})
+
+# Details of task
+def task_details(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect('all_tasks')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todos/task_form.html', {'form': form})
